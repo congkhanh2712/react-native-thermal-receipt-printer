c1248d20d134f9163d72226e0e8d9215a77f9b67
Khanh <kentho0101@gmail.com>
2021-11-23 08:52:22 +0700

    aligncenter printImage android

--- android/src/main/java/com/pinmi/react/printer/adapter/NetPrinterAdapter.java
+++ android/src/main/java/com/pinmi/react/printer/adapter/NetPrinterAdapter.java
@@ -40,20 +40,20 @@ public class NetPrinterAdapter implements PrinterAdapter {
     private String LOG_TAG = "RNNetPrinter";
     private NetPrinterDevice mNetDevice;
 
-    //    {TODO- support other ports later}
-//    private int[] PRINTER_ON_PORTS = {515, 3396, 9100, 9303};
+    // {TODO- support other ports later}
+    // private int[] PRINTER_ON_PORTS = {515, 3396, 9100, 9303};
 
-    private int[] PRINTER_ON_PORTS = {9100};
+    private int[] PRINTER_ON_PORTS = { 9100 };
     private static final String EVENT_SCANNER_RESOLVED = "scannerResolved";
     private static final String EVENT_SCANNER_RUNNING = "scannerRunning";
 
-    private static int[] p0 = new int[]{0, 128};
-    private static int[] p1 = new int[]{0, 64};
-    private static int[] p2 = new int[]{0, 32};
-    private static int[] p3 = new int[]{0, 16};
-    private static int[] p4 = new int[]{0, 8};
-    private static int[] p5 = new int[]{0, 4};
-    private static int[] p6 = new int[]{0, 2};
+    private static int[] p0 = new int[] { 0, 128 };
+    private static int[] p1 = new int[] { 0, 64 };
+    private static int[] p2 = new int[] { 0, 32 };
+    private static int[] p3 = new int[] { 0, 16 };
+    private static int[] p4 = new int[] { 0, 8 };
+    private static int[] p5 = new int[] { 0, 4 };
+    private static int[] p6 = new int[] { 0, 2 };
 
     private Socket mSocket;
 
@@ -79,7 +79,8 @@ public class NetPrinterAdapter implements PrinterAdapter {
 
     @Override
     public List<PrinterDevice> getDeviceList(Callback errorCallback) {
-        // errorCallback.invoke("do not need to invoke get device list for net printer");
+        // errorCallback.invoke("do not need to invoke get device list for net
+        // printer");
         // Use emitter instancee get devicelist to non block main thread
         this.scan();
         List<PrinterDevice> printerDevices = new ArrayList<>();
@@ -87,7 +88,8 @@ public class NetPrinterAdapter implements PrinterAdapter {
     }
 
     private void scan() {
-        if (isRunning) return;
+        if (isRunning)
+            return;
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -95,16 +97,18 @@ public class NetPrinterAdapter implements PrinterAdapter {
                     isRunning = true;
                     emitEvent(EVENT_SCANNER_RUNNING, isRunning);
 
-                    WifiManager wifiManager = (WifiManager) mContext.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                    WifiManager wifiManager = (WifiManager) mContext.getApplicationContext()
+                            .getSystemService(Context.WIFI_SERVICE);
                     String ipAddress = ipToString(wifiManager.getConnectionInfo().getIpAddress());
                     WritableArray array = Arguments.createArray();
 
-
                     String prefix = ipAddress.substring(0, ipAddress.lastIndexOf('.') + 1);
-                    int suffix = Integer.parseInt(ipAddress.substring(ipAddress.lastIndexOf('.') + 1, ipAddress.length()));
+                    int suffix = Integer
+                            .parseInt(ipAddress.substring(ipAddress.lastIndexOf('.') + 1, ipAddress.length()));
 
                     for (int i = 0; i <= 255; i++) {
-                        if (i == suffix) continue;
+                        if (i == suffix)
+                            continue;
                         ArrayList<Integer> ports = getAvailablePorts(prefix + i);
                         if (!ports.isEmpty()) {
                             WritableMap payload = Arguments.createMap();
@@ -130,47 +134,44 @@ public class NetPrinterAdapter implements PrinterAdapter {
 
     private void emitEvent(String eventName, Object data) {
         if (mContext != null) {
-            mContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                    .emit(eventName, data);
+            mContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, data);
         }
     }
 
     private ArrayList<Integer> getAvailablePorts(String address) {
         ArrayList<Integer> ports = new ArrayList<>();
         for (int port : PRINTER_ON_PORTS) {
-            if (crunchifyAddressReachable(address, port)) ports.add(port);
+            if (crunchifyAddressReachable(address, port))
+                ports.add(port);
         }
         return ports;
     }
 
-
     private static boolean crunchifyAddressReachable(String address, int port) {
-       try {
+        try {
 
-           try (Socket crunchifySocket = new Socket()) {
-               // Connects this socket to the server with a specified timeout value.
-               crunchifySocket.connect(new InetSocketAddress(address, port), 100);
-           }
-           // Return true if connection successful
-           return true;
-       } catch (IOException exception) {
-           exception.printStackTrace();
-           return false;
-       }
+            try (Socket crunchifySocket = new Socket()) {
+                // Connects this socket to the server with a specified timeout value.
+                crunchifySocket.connect(new InetSocketAddress(address, port), 100);
+            }
+            // Return true if connection successful
+            return true;
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            return false;
+        }
     }
 
     private String ipToString(int ip) {
-        return (ip & 0xFF) + "." +
-                ((ip >> 8) & 0xFF) + "." +
-                ((ip >> 16) & 0xFF) + "." +
-                ((ip >> 24) & 0xFF);
+        return (ip & 0xFF) + "." + ((ip >> 8) & 0xFF) + "." + ((ip >> 16) & 0xFF) + "." + ((ip >> 24) & 0xFF);
     }
 
     @Override
     public void selectDevice(PrinterDeviceId printerDeviceId, Callback sucessCallback, Callback errorCallback) {
         NetPrinterDeviceId netPrinterDeviceId = (NetPrinterDeviceId) printerDeviceId;
 
-        if (this.mSocket != null && !this.mSocket.isClosed() && mNetDevice.getPrinterDeviceId().equals(netPrinterDeviceId)) {
+        if (this.mSocket != null && !this.mSocket.isClosed()
+                && mNetDevice.getPrinterDeviceId().equals(netPrinterDeviceId)) {
             Log.i(LOG_TAG, "already selected device, do not need repeat to connect");
             sucessCallback.invoke(this.mNetDevice.toRNWritableMap());
             return;
@@ -184,7 +185,8 @@ public class NetPrinterAdapter implements PrinterAdapter {
                 this.mNetDevice = new NetPrinterDevice(netPrinterDeviceId.getHost(), netPrinterDeviceId.getPort());
                 sucessCallback.invoke(this.mNetDevice.toRNWritableMap());
             } else {
-                errorCallback.invoke("unable to build connection with host: " + netPrinterDeviceId.getHost() + ", port: " + netPrinterDeviceId.getPort());
+                errorCallback.invoke("unable to build connection with host: " + netPrinterDeviceId.getHost()
+                        + ", port: " + netPrinterDeviceId.getPort());
                 return;
             }
         } catch (IOException e) {
@@ -236,37 +238,39 @@ public class NetPrinterAdapter implements PrinterAdapter {
     }
 
     @Override
-    public void printImage(String image,int width, Callback errorCallback) {
-       if (this.mSocket == null) {
-           errorCallback.invoke("bluetooth connection is not built, may be you forgot to connectPrinter");
-           return;
-       }
+    public void printImage(String image, int width, Callback errorCallback) {
+        if (this.mSocket == null) {
+            errorCallback.invoke("bluetooth connection is not built, may be you forgot to connectPrinter");
+            return;
+        }
         Log.v(LOG_TAG, "image is:  " + image);
         byte[] decodeBase64ImageString = Base64.decode(image, Base64.DEFAULT);
         Bitmap bitmapImage = BitmapFactory.decodeByteArray(decodeBase64ImageString, 0, decodeBase64ImageString.length);
-        Log.d("NetPrinterModule", "decodeBase64ImageString is:  " + decodeBase64ImageString
-                + " and bitmapImage: " + bitmapImage + " and width is: " + width);
+        Log.d("NetPrinterModule", "decodeBase64ImageString is:  " + decodeBase64ImageString + " and bitmapImage: "
+                + bitmapImage + " and width is: " + width);
 
-        if(bitmapImage !=null){
-            bitmapImage = resizeImage(bitmapImage,width,false);
-            final byte[] cutPrinter = selectCutPagerModerAndCutPager(66,1);
-            final byte[] data = rasterBmpToSendData(0,bitmapImage, width);
+        if (bitmapImage != null) {
+            bitmapImage = resizeImage(bitmapImage, width, false);
+            final byte[] cutPrinter = selectCutPagerModerAndCutPager(66, 1);
+            final byte[] data = rasterBmpToSendData(0, bitmapImage, width);
+            final byte[] alignCenter = new byte[] { 0x1B, 0x61, 0x01 };
             final Socket socket = this.mSocket;
             new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try {
                         OutputStream printerOutputStream = socket.getOutputStream();
-                        printerOutputStream.write(data,0,data.length);
-                        printerOutputStream.write(cutPrinter,0,cutPrinter.length);
+                        printerOutputStream.write(data, 0, data.length);
+                        printerOutputStream.write(alignCenter, 0, alignCenter.length);
+                        printerOutputStream.write(cutPrinter, 0, cutPrinter.length);
                         printerOutputStream.flush();
                     } catch (IOException e) {
-                        Log.e(LOG_TAG, "failed to print image" );
+                        Log.e(LOG_TAG, "failed to print image");
                         e.printStackTrace();
                     }
                 }
             }).start();
-        } else{
+        } else {
             Log.d("NetPrinterModule", "bitmapImage is null");
             return;
         }
@@ -274,21 +278,17 @@ public class NetPrinterAdapter implements PrinterAdapter {
     }
 
     public static byte[] initImageCommand(int bytesByLine, int bitmapHeight) {
-        int
-                xH = bytesByLine / 256,
-                xL = bytesByLine - (xH * 256),
-                yH = bitmapHeight / 256,
+        int xH = bytesByLine / 256, xL = bytesByLine - (xH * 256), yH = bitmapHeight / 256,
                 yL = bitmapHeight - (yH * 256);
 
         byte[] imageBytes = new byte[8 + bytesByLine * bitmapHeight];
-        System.arraycopy(new byte[]{0x1D, 0x76, 0x30, 0x00, (byte) xL, (byte) xH, (byte) yL, (byte) yH}, 0, imageBytes, 0, 8);
+        System.arraycopy(new byte[] { 0x1D, 0x76, 0x30, 0x00, (byte) xL, (byte) xH, (byte) yL, (byte) yH }, 0,
+                imageBytes, 0, 8);
         return imageBytes;
     }
 
     public static byte[] rasterBmpToSendData(final int m, final Bitmap mBitmap, final int pagewidth) {
-        int
-                bitmapWidth = mBitmap.getWidth(),
-                bitmapHeight = mBitmap.getHeight(),
+        int bitmapWidth = mBitmap.getWidth(), bitmapHeight = mBitmap.getHeight(),
                 bytesByLine = (int) Math.ceil(((float) bitmapWidth) / 8f);
 
         byte[] imageBytes = initImageCommand(bytesByLine, bitmapHeight);
@@ -300,9 +300,7 @@ public class NetPrinterAdapter implements PrinterAdapter {
                 for (int k = 0; k < 8; k++) {
                     int posX = j + k;
                     if (posX < bitmapWidth) {
-                        int color = mBitmap.getPixel(posX, posY),
-                                r = (color >> 16) & 0xff,
-                                g = (color >> 8) & 0xff,
+                        int color = mBitmap.getPixel(posX, posY), r = (color >> 16) & 0xff, g = (color >> 8) & 0xff,
                                 b = color & 0xff;
 
                         if (r > 160 && g > 160 && b > 160) {
@@ -330,7 +328,7 @@ public class NetPrinterAdapter implements PrinterAdapter {
         final ColorMatrix cm = new ColorMatrix();
         cm.setSaturation(0.0f);
         final ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
-        paint.setColorFilter((ColorFilter)f);
+        paint.setColorFilter((ColorFilter) f);
         c.drawBitmap(bmpOriginal, 0.0f, 0.0f, paint);
         return bmpGrayscale;
     }
@@ -355,8 +353,7 @@ public class NetPrinterAdapter implements PrinterAdapter {
                 if (g >= 128) {
                     pixels[width * k + l] = -1;
                     e = g - 255;
-                }
-                else {
+                } else {
                     pixels[width * k + l] = -16777216;
                     e = g - 0;
                 }
@@ -370,13 +367,11 @@ public class NetPrinterAdapter implements PrinterAdapter {
                     final int[] array3 = gray;
                     final int n3 = width * (k + 1) + l + 1;
                     array3[n3] += e / 4;
-                }
-                else if (l == width - 1 && k < height - 1) {
+                } else if (l == width - 1 && k < height - 1) {
                     final int[] array4 = gray;
                     final int n4 = width * (k + 1) + l;
                     array4[n4] += 3 * e / 8;
-                }
-                else if (l < width - 1 && k == height - 1) {
+                } else if (l < width - 1 && k == height - 1) {
                     final int[] array5 = gray;
                     final int n5 = width * k + l + 1;
                     array5[n5] += e / 4;
@@ -398,13 +393,12 @@ public class NetPrinterAdapter implements PrinterAdapter {
                     if ((b[y * w + x] & 0xFF0000) >> 16 != 0) {
                         final byte[] array = data;
                         final int n2 = y * n + x / 8;
-                        array[n2] |= (byte)(mask << 7 - x % 8);
+                        array[n2] |= (byte) (mask << 7 - x % 8);
                     }
-                }
-                else if (x >= w) {
+                } else if (x >= w) {
                     final byte[] array2 = data;
                     final int n3 = y * n + x / 8;
-                    array2[n3] |= (byte)(mask << 7 - x % 8);
+                    array2[n3] |= (byte) (mask << 7 - x % 8);
                 }
             }
         }
@@ -415,11 +409,10 @@ public class NetPrinterAdapter implements PrinterAdapter {
     }
 
     public static byte[] setAbsolutePrintPosition(final int m, final int n) {
-        final byte[] data = { 27, 36, (byte)m, (byte)n };
+        final byte[] data = { 27, 36, (byte) m, (byte) n };
         return data;
     }
 
-    
     public static byte[] initializePrinter() {
         final byte[] data = { 27, 64 };
         return data;
@@ -429,23 +422,22 @@ public class NetPrinterAdapter implements PrinterAdapter {
         if (m != 66) {
             return new byte[0];
         }
-        final byte[] data = { 29, 86, (byte)m, (byte)n };
+        final byte[] data = { 29, 86, (byte) m, (byte) n };
         return data;
     }
 
-    public static Bitmap resizeImage(Bitmap bitmap, int w,boolean ischecked)
-    {
+    public static Bitmap resizeImage(Bitmap bitmap, int w, boolean ischecked) {
 
         Bitmap BitmapOrg = bitmap;
         Bitmap resizedBitmap = null;
         int width = BitmapOrg.getWidth();
         int height = BitmapOrg.getHeight();
-        if (width<=w) {
+        if (width <= w) {
             return bitmap;
         }
         if (!ischecked) {
             int newWidth = w;
-            int newHeight = height*w/width;
+            int newHeight = height * w / width;
 
             float scaleWidth = ((float) newWidth) / width;
             float scaleHeight = ((float) newHeight) / height;
@@ -454,13 +446,11 @@ public class NetPrinterAdapter implements PrinterAdapter {
             matrix.postScale(scaleWidth, scaleHeight);
             // if you want to rotate the Bitmap
             // matrix.postRotate(45);
-            resizedBitmap = Bitmap.createBitmap(BitmapOrg, 0, 0, width,
-                    height, matrix, true);
-        }else {
-            resizedBitmap=Bitmap.createBitmap(BitmapOrg, 0, 0, w, height);
+            resizedBitmap = Bitmap.createBitmap(BitmapOrg, 0, 0, width, height, matrix, true);
+        } else {
+            resizedBitmap = Bitmap.createBitmap(BitmapOrg, 0, 0, w, height);
         }
 
         return resizedBitmap;
     }
 }
-
